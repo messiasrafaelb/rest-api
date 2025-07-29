dd830fab92d588e264670c912c6ad04d5b251794
Rafael <rafaelbernardes201@gmail.com>
2025-07-29 08:59:39 -0300

    feat(repositories): creates the ProductRepository interface

---
+++ src/main/java/com/example/rest_api/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.rest_api.repositories;
+
+import com.example.rest_api.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
