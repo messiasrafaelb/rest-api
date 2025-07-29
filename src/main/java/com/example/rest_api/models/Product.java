33cf2c30d6f199cc79f1ecde50d2f315bc5c8845
Rafael <rafaelbernardes201@gmail.com>
2025-07-29 08:44:55 -0300

    feat(models): creates the Product class

---
+++ src/main/java/com/example/rest_api/models/Product.java
@@ -0,0 +1,36 @@
+package com.example.rest_api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @ToString.Include
+    @Column(nullable = false)
+    private String name;
+
+    @ToString.Include
+    @Column(length = 150)
+    private String description;
+
+    @NonNull
+    @ToString.Include
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+}
