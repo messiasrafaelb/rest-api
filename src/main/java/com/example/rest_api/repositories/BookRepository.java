cb8ad30e45cd1718cc0d6d6840231d620a9a48a5
Rafael <rafaelbernardes201@gmail.com>
2025-07-29 00:18:50 -0300

    feat(repositories): creates the BookRepository interface

---
+++ src/main/java/com/example/rest_api/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.rest_api.repositories;
+
+import com.example.rest_api.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
