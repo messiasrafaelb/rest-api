27b20e425845ba8b9b358544c30e84801528590b
Rafael <rafaelbernardes201@gmail.com>
2025-07-30 23:40:39 -0300

    feat(dtos): creates the ProductRequestDTO class

---
+++ src/main/java/com/example/rest_api/dtos/ProductRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.rest_api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class ProductRequestDTO {
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+}
