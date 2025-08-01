8a2760636570a3d1c89890cd1b990ffdcceb9a8e
Rafael <rafaelbernardes201@gmail.com>
2025-08-01 13:20:44 -0300

    feat(controllers): adds validation in methods in ProductController class

--- src/main/java/com/example/rest_api/controllers/ProductController.java
+++ src/main/java/com/example/rest_api/controllers/ProductController.java
@@ -3,12 +3,16 @@ package com.example.rest_api.controllers;
 import com.example.rest_api.dtos.ProductRequestDTO;
 import com.example.rest_api.dtos.ProductResponseDTO;
 import com.example.rest_api.services.ProductService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -27,24 +31,24 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
+    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable @Min(1) Long id) {
         var product = service.getProductById(id);
         return ResponseEntity.ok(product);
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequest) {
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO productRequest) {
         return ResponseEntity.ok(service.createProduct(productRequest));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id,
-                                                            @RequestBody ProductRequestDTO productRequest) {
+    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable @Min(1) Long id,
+                                                            @Valid @RequestBody ProductRequestDTO productRequest) {
         return ResponseEntity.ok(service.updateProduct(id, productRequest));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable @Min(1) Long id) {
         service.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
