5b42e7b9394ad6b9053b1d446a940ea5dae35f5c
Rafael <rafaelbernardes201@gmail.com>
2025-08-01 13:11:15 -0300

    feat(dtos): adds validations on fields in ProductRequestDTO class

--- src/main/java/com/example/rest_api/dtos/ProductRequestDTO.java
+++ src/main/java/com/example/rest_api/dtos/ProductRequestDTO.java
@@ -1,5 +1,9 @@
 package com.example.rest_api.dtos;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +12,16 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 public class ProductRequestDTO {
+
+    @NotBlank
+    @Size(max = 100)
     private String name;
+
+    @NotBlank
+    @Size(max = 300)
     private String description;
+
+    @Positive
+    @Digits(integer = 9, fraction = 2)
     private BigDecimal price;
 }
