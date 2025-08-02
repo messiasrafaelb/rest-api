b26c22942968bfc12f2eb353f6a8279eda59e784
Rafael <rafaelbernardes201@gmail.com>
2025-08-02 18:05:14 -0300

    feat(exceptions): creates the ErrorResponse class

---
+++ src/main/java/com/example/rest_api/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.rest_api.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.List;
+
+@Builder
+public class ErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Sao_Paulo")
+    private Instant timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<String> errors;
+}
