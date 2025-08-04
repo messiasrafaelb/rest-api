b399c3d6b72748fa97139e3103ac4e2eaae981b8
Rafael <rafaelbernardes201@gmail.com>
2025-08-04 11:24:48 -0300

    feat(controllers): adds HATEOAS to getProductById in ProductController class

--- src/main/java/com/example/rest_api/controllers/ProductController.java
+++ src/main/java/com/example/rest_api/controllers/ProductController.java
@@ -3,16 +3,18 @@ package com.example.rest_api.controllers;
 import com.example.rest_api.dtos.ProductRequestDTO;
 import com.example.rest_api.dtos.ProductResponseDTO;
 import com.example.rest_api.services.ProductService;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Validated
 @RestController
@@ -33,10 +35,14 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable
+    public ResponseEntity<EntityModel<ProductResponseDTO>> getProductById(@PathVariable
                                                              @Min(value = 1, message = "{product.id.min}") Long id) {
         var product = service.getProductById(id);
-        return ResponseEntity.ok(product);
+        var model = EntityModel.of(product);
+        model.add(linkTo(methodOn(ProductController.class).getProductById(id)).withSelfRel());
+        model.add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("all-products"));
+        model.add(linkTo(methodOn(ProductController.class).deleteProduct(id)).withRel("delete"));
+        return ResponseEntity.ok(model);
     }
 
     @PostMapping
