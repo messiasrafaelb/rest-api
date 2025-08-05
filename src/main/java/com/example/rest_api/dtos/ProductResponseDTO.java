73771f5afeb4f51d94990b9742fb71333ea2d771
rafael <messiasrafaelb@gmail.com>
2025-08-05 11:34:40 -0300

    feat(dtos): adds id field with @JsonIgnore annotation

--- src/main/java/com/example/rest_api/dtos/ProductResponseDTO.java
+++ src/main/java/com/example/rest_api/dtos/ProductResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.rest_api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,8 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 public class ProductResponseDTO {
+    @JsonIgnore
+    private Long id;
     private String name;
     private BigDecimal price;
 }
