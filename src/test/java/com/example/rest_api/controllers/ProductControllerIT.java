7bc445945e1000eed057bf65e18d54785bc402e1
rafael <messiasrafaelb@gmail.com>
2025-08-08 12:14:06 -0300

    feat(test/controller): creates ProductControllerIT class for integration tests

---
+++ src/test/java/com/example/rest_api/controllers/ProductControllerIT.java
@@ -0,0 +1,17 @@
+package com.example.rest_api.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductControllerIT {
+
+
+
+
+
+
+
+
+
+
+}
