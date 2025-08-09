ca337ba0cbcb06d4f507900961d5123d4c7a6d37
rafael <messiasrafaelb@gmail.com>
2025-08-09 20:05:02 -0300

    refactor(dtos): add Relation(collectionRelation = 'products') to reference a list in jsonPath

--- src/main/java/com/example/rest_api/dtos/ProductResponseDTO.java
+++ src/main/java/com/example/rest_api/dtos/ProductResponseDTO.java
@@ -3,11 +3,13 @@ package com.example.rest_api.dtos;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@Relation(collectionRelation = "products")
 public class ProductResponseDTO {
     @JsonIgnore
     private Long id;
