de37bc38d6cd0ddf2231cd7edec9fe0cfdf975da
rafael <messiasrafaelb@gmail.com>
2025-08-09 20:07:25 -0300

    fix(exceptions): add Getter annotation to ErrorResponse class

--- src/main/java/com/example/rest_api/exceptions/ErrorResponse.java
+++ src/main/java/com/example/rest_api/exceptions/ErrorResponse.java
@@ -3,10 +3,12 @@ package com.example.rest_api.exceptions;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.Instant;
 import java.util.List;
 
+@Getter
 @Builder
 public class ErrorResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Sao_Paulo")
